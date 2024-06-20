5c6e76bbef0335ab36187ead47ade6dc789c145c
Ekaterina Gurina <Ekaterina.Gurina@skoltech.ru>
2024-06-20 23:46:25 +0300

    Files for hw5

---
+++ hw5/DAG_for_refit.py
@@ -0,0 +1,126 @@
+import uuid
+from datetime import datetime, timedelta
+from airflow import DAG, settings
+from airflow.models import Connection, Variable
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.python import PythonVirtualenvOperator
+from airflow.providers.yandex.operators.yandexcloud_dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocCreatePysparkJobOperator,
+    DataprocDeleteClusterOperator,
+
+)
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+
+# Common settings for your environment
+YC_DP_FOLDER_ID = 'b1g257asjsuj1uf0o7ao'
+YC_DP_SUBNET_ID = 'e9bkhau11ovt54g1cnt6'
+YC_DP_SA_ID = 'ajepilq3j2e0j34f65qf'
+YC_DP_AZ = 'ru-central1-a'
+YC_DP_SSH_PUBLIC_KEY = Variable.get("SSH_PUBLIC")
+
+# Settings for S3 buckets
+YC_SOURCE_BUCKET = 'bucket-mlops-fraud-system'     # YC S3 bucket for pyspark source files
+YC_DP_LOGS_BUCKET = 'bucket-mlops-fraud-system/airflow_logs/'      # YC S3 bucket for Data Proc cluster logs
+
+
+
+# Создание подключения для Object Storage
+session = settings.Session()
+ycS3_connection = Connection(
+    conn_id='yc-s3',
+    conn_type='s3',
+    host='https://storage.yandexcloud.net/',
+    extra={
+        "aws_access_key_id": Variable.get("S3_KEY_ID"),
+        "aws_secret_access_key": Variable.get("S3_SECRET_KEY"),
+        "host": "https://storage.yandexcloud.net/"
+    }
+)
+
+
+if not session.query(Connection).filter(Connection.conn_id == ycS3_connection.conn_id).first():
+    session.add(ycS3_connection)
+    session.commit()
+
+ycSA_connection = Connection(
+    conn_id='yc-SA',
+    conn_type='yandexcloud',
+    extra={
+        "extra__yandexcloud__public_ssh_key": Variable.get("DP_PUBLIC_SSH_KEY"),
+        "extra__yandexcloud__service_account_json_path": Variable.get("DP_SA_PATH")
+    }
+)
+
+if not session.query(Connection).filter(Connection.conn_id == ycSA_connection.conn_id).first():
+    session.add(ycSA_connection)
+    session.commit()
+
+
+# Настройки DAG
+with DAG(
+        dag_id = 'DATA_PREPROCESS_3',
+        schedule_interval='@once',
+        start_date=datetime(year = 2024,month = 6,day = 20),
+        #schedule_interval = timedelta(minutes=16),
+        catchup=False
+) as ingest_dag:
+
+    create_spark_cluster = DataprocCreateClusterOperator(
+        task_id='dp-cluster-create-task',
+        folder_id=YC_DP_FOLDER_ID,
+        cluster_name=f'tmp-dp-{uuid.uuid4()}',
+        cluster_description='Temporary cluster for Spark processing under Airflow orchestration',
+        subnet_id=YC_DP_SUBNET_ID,
+        s3_bucket=YC_DP_LOGS_BUCKET,
+        service_account_id=YC_DP_SA_ID,
+        ssh_public_keys=YC_DP_SSH_PUBLIC_KEY,
+        zone=YC_DP_AZ,
+        cluster_image_version='2.0.43',
+        masternode_resource_preset='s3-c2-m8',
+        masternode_disk_type='network-ssd',
+        masternode_disk_size=20,
+        datanode_resource_preset='s3-c4-m16',
+        datanode_disk_type='network-ssd',
+        datanode_disk_size=80,
+        datanode_count=2,
+        services=['YARN', 'SPARK', 'HDFS', 'MAPREDUCE'],  
+        computenode_count=0,           
+        connection_id=ycSA_connection.conn_id,
+        dag=ingest_dag,
+        
+    )
+
+
+
+
+    # # 3 этап: запуск задания PySpark расчет
+    # poke_spark_processing = DataprocCreatePysparkJobOperator(
+    #     task_id='dp-cluster-pyspark-task',
+    #     main_python_file_uri=f's3a://bucket-mlops-fraud-system/scripts/model_reffit.py',
+    #     jar_file_uris = [f's3a://bucket-mlops-fraud-system/scripts/mlflow-spark-1.27.0.jar'],
+    #     connection_id = ycSA_connection.conn_id,
+    #     dag=ingest_dag,
+    # )    
+        # 3 этап: запуск задания PySpark расчет
+    poke_spark_processing = DataprocCreatePysparkJobOperator(
+        task_id='dp-cluster-pyspark-task',
+        main_python_file_uri=f's3a://bucket-mlops-fraud-system/scripts/model_reffit.py',
+        #jar_file_uris = [f's3a://bucket-mlops-fraud-system/scripts/mlflow-spark-1.27.0.jar'],
+        connection_id = ycSA_connection.conn_id,
+        dag=ingest_dag,
+        properties = {'spark.submit.deployMode': 'cluster',
+                    'spark.yarn.dist.archives': f's3a://{YC_SOURCE_BUCKET}/pyspark_venv.tar.gz#venv1',
+                    'spark.yarn.appMasterEnv.PYSPARK_PYTHON': './venv1/bin/python',
+                    'spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON': './venv1/bin/python'}
+    )  
+
+
+    delete_spark_cluster = DataprocDeleteClusterOperator(
+        task_id='dp-cluster-delete-task',
+        trigger_rule=TriggerRule.ALL_DONE,
+        dag=ingest_dag
+    )
+    # # Формирование DAG из указанных выше этапов
+    create_spark_cluster >>poke_spark_processing>>delete_spark_cluster
---
+++ hw5/model_reffit.py
@@ -0,0 +1,131 @@
+import findspark
+findspark.init()
+
+import os
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
+from pyspark.ml.classification import GBTClassifier
+from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.ml import Pipeline
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+from pyspark.sql.functions import col, when, rand
+
+SELECTED_FEATURES = [#'tranaction_id',
+                                         #'tx_datetime',
+                                         'customer_id',
+                                         'terminal_id',
+                                         'tx_amount',
+                                         #'tx_time_seconds',
+                                         #'tx_time_days',
+                                         #'tx_fraud',
+                                         #'tx_fraud_scenario',
+                                         'day_of_week',
+                                         'week_of_year',
+                                         'is_weekend',
+                                         'hour',
+                                         'is_day',
+                                         'avg_amount_per_customer_for_1_days',
+                                         'number_of_tx_per_customer_for_1_days',
+                                         'avg_amount_per_customer_for_7_days',
+                                         'number_of_tx_per_customer_for_7_days',
+                                         'avg_amount_per_customer_for_30_days',
+                                         'number_of_tx_per_customer_for_30_days',
+                                         #'number_of_tx_per_terminal_for_1_day',
+                                         #'number_of_fraud_tx_per_terminal_for_1_day',
+                                         #'number_of_tx_per_terminal_for_7_day',
+                                         #'number_of_fraud_tx_per_terminal_for_7_day',
+                                         #'number_of_tx_per_terminal_for_30_day',
+                                         #'number_of_fraud_tx_per_terminal_for_30_day'
+]
+
+FILES_FOR_TRAINING = ['2019-08-22','2019-09-21', '2019-10-21']
+FILES_FOR_TESTING = ['2019-11-20']
+
+SOURCE_BUCKET = 'bucket-mlops-fraud-system/cleaned_data/' 
+S3_KEY_ID = 'YCAJERcdEYXXGtibDA_bKmuCN'
+S3_SECRET_KEY = 'YCOhTcO5kxCoBY950-36WcWo6uzy8tBJ4S1gxEsP'
+TRACKING_SERVER_HOST = '178.154.205.20'
+
+def get_pipeline():
+    numericAssembler = VectorAssembler()\
+                        .setInputCols(SELECTED_FEATURES)\
+                        .setOutputCol("features")
+    
+    scaler = MinMaxScaler()\
+            .setInputCol("features")\
+            .setOutputCol("scaledFeatures")
+    
+    classifier = GBTClassifier(featuresCol='scaledFeatures',
+                    labelCol='tx_fraud',
+                    maxDepth = 3,
+                    weightCol = "classWeights",
+                    subsamplingRate = 0.8
+                    )
+    pipeline = Pipeline(stages=[numericAssembler, scaler, classifier])
+    return pipeline
+
+def calculate_metric_values(predictions):
+    predictions = predictions.withColumn('tx_fraud', predictions['tx_fraud'].cast('double'))
+    predictions = predictions.withColumn('prediction', predictions['prediction'].cast('double'))
+
+    results = predictions.select(['prediction', 'tx_fraud'])
+    predictionAndLabels=results.rdd
+    metrics = MulticlassMetrics(predictionAndLabels)
+
+    cm=metrics.confusionMatrix().toArray()
+
+    precision=(cm[0][0])/(cm[0][0]+cm[1][0])
+    recall=(cm[0][0])/(cm[0][0]+cm[0][1])
+    return precision, recall
+
+def make_unioned_df(file_names):
+    for i, fname in enumerate(file_names):
+        data = spark.read.load(f's3a://{SOURCE_BUCKET}/clean_{fname}.parquet')
+        data = data.withColumn("classWeights", when(data.tx_fraud==1, 5).otherwise(1))
+        if i == 0:
+            unioned_df = data
+        else:
+            unioned_df = unioned_df.union(data) 
+
+    return unioned_df
+
+
+
+spark = SparkSession.builder \
+    .appName("model_reffit") \
+    .getOrCreate()
+
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "https://storage.yandexcloud.net"
+os.environ["AWS_ACCESS_KEY_ID"] = S3_KEY_ID
+os.environ["AWS_SECRET_ACCESS_KEY"] = S3_SECRET_KEY
+
+mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:8000")
+mlflow.set_experiment("pyspark_experiment_3")
+    
+inf_pipeline = get_pipeline()
+
+train_unioned_df = make_unioned_df(FILES_FOR_TRAINING)
+model = inf_pipeline.fit(train_unioned_df)
+
+predictions_train = model.transform(train_unioned_df)
+precision_train, recall_train = calculate_metric_values(predictions_train)
+# print(precision_train, recall_train)
+
+mlflow.log_metric("Precision on train", precision)
+mlflow.log_metric("Recall on train", recall)
+
+test_unioned_df = make_unioned_df(FILES_FOR_TESTING)
+predictions_test = model.transform(test_unioned_df)
+precision_test, recall_test = calculate_metric_values(predictions_test)
+# print(precision_test, recall_test)
+
+
+mlflow.log_metric("Precision on test", precision_test)
+mlflow.log_metric("Recall on test", recall_test)
+
+
