32371ff183d4fc7b413ed3ab8f9a66db06a4ad01
Ekaterina Gurina <Ekaterina.Gurina@skoltech.ru>
2024-08-03 11:01:18 +0300

    service_monitor added

--- app/app.py
+++ app/app.py
@@ -35,7 +35,7 @@ app.add_route("/metrics", handle_metrics)
 def load_model():
     MODEL.model = ModelInference()
 
-@app.post("/healthcheck")
+@app.get("/healthcheck")
 def read_healthcheck():
     return {"status": "healthcheck"}
 
